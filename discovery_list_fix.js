e27156ffb69ad2c6a2bfbf42dca16f6d6815f3b9
Mike Funk <mike@mikefunk.com>
2012-07-21 18:06:17 -0400

    added js

---
+++ discovery_list_fix.js
@@ -0,0 +1,32 @@
+(function(){
+
+	// the minimum version of jQuery we want
+	var v = "1.7.2";
+
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			// your JavaScript code goes here!
+
+			$("#sliderTitle").html($("#sliderTitle option").sort(function (a, b) {
+			    return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
+			}));
+		})();
+	}
+
+})();
